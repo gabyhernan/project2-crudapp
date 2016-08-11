4c54ba4e8eddd850b94e696d4072a16b4f5fe86f
Gabriella Hernandez <gabyhernandezlebron@gmail.com>
2016-08-11 18:16:51 -0400

    adds background profile for heroku

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const pgp = require('pg-promise')();
+
+const db = pgp('postgres://GAB@localhost:5432/auth');
+
 const app = express();
 const mustacheExpress = require('mustache-express');
 const bodyParser = require("body-parser");
 const session = require('express-session');
 const flash = require('connect-flash');
+
+
 
 app.engine('html', mustacheExpress());
 app.set('view engine', 'html');
@@ -12,7 +18,8 @@ app.use("/", express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(session({
+
+app.use(session({ // uses session code
   secret: 'demo-secret',
   resave: false,
   saveUninitialized: true,
@@ -21,7 +28,23 @@ app.use(session({
 
 app.use(flash());
 
-app.listen(3000, function () {
+app.get('/', function(req,res){ // gets main page
+  var logged_in ,
+      email;
+
+      if(req.session.user){
+        logged_in = true;
+        email = req.session.user.email;
+      }
+  var data = {
+    'logged_in': logged_in,
+    'email': email
+  }
+  res.render('index', data);
+});
+
+
+app.listen(3000, function () { // listen to local server
   console.log('Auth Demo App Online!');
 });
 
