76f41bd4a31896d8ba54f09390090ac3c33e8cd5
Gabriella Hernandez <gabyhernandezlebron@gmail.com>
2016-08-15 21:43:27 -0400

    pinterest layout

--- public/js/background.js
+++ public/js/background.js
@@ -1,8 +1,20 @@
 $(document).ready(function() {
-
-$(".favorite-button").click(function() {
-  $(".favorite-art").toggleClass("sliding-down");
-  $(this).toggleClass("button-active");
+$('button').on('click', function(){
+  $(this).toggleClass('faved');
 });
 
+// Dropdown toggle from http://codepen.io/scottb/pen/gixIv
+$('.dropdown-toggle').click(function(){
+  $(this).next('.dropdown').toggle();
 });
+
+$(document).click(function(e) {
+  var target = e.target;
+  if (!$(target).is('.dropdown-toggle') && !$(target).parents().is('.dropdown-toggle')) {
+    $('.dropdown').hide();
+  }
+});
+
+
+
+  });
